a62c3fa17fc56445343f02f2d8271b2356adaa6a
jsjoblom79 <jsjoblom@me.com>
2024-09-16 14:09:41 -0600

    Add project files.

---
+++ GetSQLUsers/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Gather info to connect to the database
+using GetSQLUsers;
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.Write("Enter the server you wish to connect to: ");
+var host = Console.ReadLine();
+Console.Write("Do you need to enter SQL Credentials? [Y] or [N]: ");
+var cred = Console.ReadLine();
+var userId = "";
+var pwd = "";
+var connectionString = "";
+//Get User Credentials if necessary
+if (cred.ToLower().Equals("y"))
+{
+    Console.Write("Enter User ID: ");
+    userId = Console.ReadLine();
+    pwd = Services.GetPassword();
+
+    connectionString = $"Server={host};Database=master;User ID={userId};Password={pwd};Trust Server Certificate=True";
+}
+else
+{
+    connectionString = $"Server={host};Database=master;Integrated Security=True;Trust Server Certificate=True";
+}
+
+Console.Write("Where do you want to place the output files: ");
+var outputFolder = Console.ReadLine();
+
+if (!string.IsNullOrEmpty(outputFolder) && !Directory.Exists(outputFolder))
+{
+    Directory.CreateDirectory(outputFolder);
+}
+Console.ForegroundColor = ConsoleColor.DarkCyan;
+Console.WriteLine($"Getting Logins for server: {host} - {DateTime.Now:MM/dd/yyyy hh:mm:ss}");
+Services.GetSQLUserScripts(connectionString, outputFolder);
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine($"Login Script Complete. {DateTime.Now:MM/dd/yyyy hh:mm:ss}");
---
+++ GetSQLUsers/Services.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.SqlServer.Management.Common;
+using Microsoft.SqlServer.Management.Sdk.Sfc;
+using Microsoft.SqlServer.Management.Smo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetSQLUsers
+{
+    public class Services
+    {
+        public static string GetPassword()
+        {
+            var pwd = "";
+
+            while (true)
+            {
+                ConsoleKeyInfo i = Console.ReadKey(true);
+                if (i.Key == ConsoleKey.Enter)
+                {
+                    break;
+                }
+                else if (i.Key == ConsoleKey.Backspace)
+                {
+                    if (pwd.Length > 0)
+                    {
+                        pwd.Remove(pwd.Length - 1);
+                        Console.Write("\b \b");
+                    }
+                }
+                else if (i.KeyChar != '\u0000')
+                {
+                    pwd += i.KeyChar;
+                    Console.Write("*");
+                }
+            }
+            return pwd;
+        }
+
+        public static void GetSQLUserScripts(string connectionString, string outputFolder)
+        {
+
+            using(SqlConnection connection = new SqlConnection(connectionString))
+            {
+                ServerConnection serverConnection = new ServerConnection(connection);
+                Server server = new Server(serverConnection);
+
+                LoginCollection logins = server.Logins;
+                var loginInfo = "";
+                foreach (Login login in logins)
+                {
+                    if (!login.IsSystemObject)
+                    {
+                        if (!login.IsDisabled)
+                        {
+                            foreach (var line in login.Script())
+                            {
+                                loginInfo += line + "\n";
+                            }
+                        }
+                    }
+                    
+
+                    loginInfo += "\n";
+                }
+
+                File.WriteAllText($"{outputFolder}\\UserLogins_{server.Name}.sql", loginInfo);
+            }
+
+        }
+    }
+}
